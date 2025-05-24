ee8b6fef7cd67857554d33b43ab6aeeb7d4630ba
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-24 06:01:48 +0200

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]),
+    FontAwesomeModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
