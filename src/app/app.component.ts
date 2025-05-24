b99d880aba19d58a10f39199074da0c7fea48a57
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-24 06:44:24 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  template: '<app-header></app-header>',
-  styles: []
+  template: `
+    <app-header></app-header>
+    <app-projects></app-projects>
+  `,
+  styles: [],
+  imports: [HeaderComponent, ProjectsComponent]
 })
 export class AppComponent {}
